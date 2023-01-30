bff404b52213d6090629d88c1a5173b71e2e854c
valenrau-yop <vraurobayo@frba.utn.edu.ar>
2023-01-30 09:15:50 -0300

    added comment

--- src/main/kotlin/com/jb/transactionvalidation/util/AmountDto.kt
+++ src/main/kotlin/com/jb/transactionvalidation/util/AmountDto.kt
@@ -1,3 +1,5 @@
 package com.jb.transactionvalidation.util
 
+// Could have use @NotNull annotation but i still had to handle the field as nullable,
+// so in order to reduce extra code, i just decided not to use it
 data class AmountDto(val amount: Int?)
